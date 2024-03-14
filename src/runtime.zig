bc9396d5fbb7e78ade270c0646f2592e28ccff93
em-dash <33614480+em-dash@users.noreply.github.com>
2024-03-14 16:49:44 +1100

    fiddle with thread

--- src/runtime.zig
+++ src/runtime.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 const types = @import("types.zig");
 const Object = types.Object;
 
+const Bytecode = struct {};
+
 /// Stack frame.
 const Frame = struct {
     /// Program counter.
@@ -16,8 +18,7 @@ const Frame = struct {
 const Thread = struct {
     call_stack: []u8,
 
-    fn init(entry_point: u32, options: Options) !Thread {
-        _ = entry_point; // autofix
+    fn init(options: Options) !Thread {
         var result: Thread = undefined;
         result.call_stack = std.heap.page_allocator.alloc(u8, options.size);
     }
