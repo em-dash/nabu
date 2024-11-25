ef366869bfa2148a6aa37ec3bedeb6c308b3e1c7
em-dash <33614480+em-dash@users.noreply.github.com>
2024-11-25 20:05:51 +1100

    fix the grammar

--- src/parsing.zig
+++ src/parsing.zig
@@ -46,10 +46,12 @@ const Node = union(enum(u8)) {
         error_union: u32,
         block: u32,
     },
-    block: struct {},
-    // type: struct {
-    //     type: u32,
-    //     qualifier: enum(u32) { none, list, map, reference },
+    block_member: struct {
+        statement: u32,
+        next: u32,
+    },
+    // block: struct {
+    //     none: u32,
     // },
     type: union(enum(u32)) {
         none: u32,
@@ -66,7 +68,7 @@ const Node = union(enum(u8)) {
         type: u32,
     },
     @"error": union(enum(u32)) {
-        none,
+        inferred,
         name: u32,
         anon: u32,
     },
@@ -97,6 +99,8 @@ const Node = union(enum(u8)) {
     // error_decl: struct {},
 };
 
+const NodeTag = @typeInfo(Node).Union.tag_type.?;
+
 pub const Ast = struct {
     nodes: std.ArrayListUnmanaged(Node),
     allocator: std.mem.Allocator,
@@ -115,8 +119,9 @@ pub const Ast = struct {
         if (result < self.tokens.len) return result else return null_index;
     }
 
-    fn addNode(self: *Ast) !u32 {
+    fn addNode(self: *Ast, comptime tag: NodeTag) !u32 {
         try self.nodes.append(self.allocator, undefined);
+        self.nodes.items[self.nodes.items.len - 1] = @unionInit(Node, @tagName(tag), undefined);
         return @as(u32, @truncate(self.nodes.items.len)) - 1;
     }
 
@@ -146,17 +151,17 @@ fn expectToken(ast: *Ast, expect: tokenization.Tag) ParseError!void {
 }
 
 fn parseScopedIdentifier(ast: *Ast) ParseError!u32 {
-    const root = try ast.addNode();
-    ast.nodes.items[root] = .{ .scoped_identifier = .{
+    const root = try ast.addNode(.scoped_identifier);
+    ast.nodes.items[root].scoped_identifier = .{
         .identifier = ast.pop(),
         .next = null_index,
-    } };
+    };
 
     var current = root;
 
     while (ast.tokens[ast.peek()].tag == .dot) {
         _ = ast.pop();
-        ast.nodes.items[current].scoped_identifier.next = try ast.addNode();
+        ast.nodes.items[current].scoped_identifier.next = try ast.addNode(.scoped_identifier);
         current = ast.nodes.items[current].scoped_identifier.next;
         const token = ast.pop();
         if (ast.tokens[token].tag != .identifier) {
@@ -171,14 +176,14 @@ fn parseScopedIdentifier(ast: *Ast) ParseError!u32 {
 }
 
 fn parseType(ast: *Ast) ParseError!u32 {
-    const node = try ast.addNode();
+    const node = try ast.addNode(.type);
 
     const first_token = ast.pop();
     switch (ast.tokens[first_token].tag) {
         .l_bracket => {
-            ast.nodes.items[node] = .{ .type = .{
+            ast.nodes.items[node].type = .{
                 .list = try parseType(ast),
-            } };
+            };
 
             try expectToken(ast, .l_bracket);
         },
@@ -186,14 +191,14 @@ fn parseType(ast: *Ast) ParseError!u32 {
             std.debug.panic("not implemented", .{});
         },
         .asterisk => {
-            ast.nodes.items[node] = .{ .type = .{
+            ast.nodes.items[node].type = .{
                 .reference = try parseType(ast),
-            } };
+            };
         },
         .identifier => {
-            ast.nodes.items[node] = .{ .type = .{
+            ast.nodes.items[node].type = .{
                 .none = try parseType(ast),
-            } };
+            };
         },
         else => {
             ast.error_payload = &ast.tokens[first_token];
@@ -209,11 +214,11 @@ fn parseFnArgs(ast: *Ast) ParseError!u32 {
     var current_node = null_index;
 
     while (ast.tokens[ast.peek()].tag != .r_paren) {
-        const node = try ast.addNode();
-        ast.nodes.items[node] = .{ .decl_param = .{
+        const node = try ast.addNode(.decl_param);
+        ast.nodes.items[node].decl_param = .{
             .identifier = ast.pop(),
             .type = undefined,
-        } };
+        };
         if (ast.tokens[ast.nodes.items[node].decl_param.identifier].tag != .identifier) {
             ast.error_payload = &ast.tokens[ast.nodes.items[node].decl_param.identifier];
             return error.UnexpectedToken;
@@ -224,19 +229,19 @@ fn parseFnArgs(ast: *Ast) ParseError!u32 {
         ast.nodes.items[node].decl_param.type = try parseType(ast);
 
         if (current_node == null_index) {
-            root_node = try ast.addNode();
+            root_node = try ast.addNode(.fn_param);
             current_node = root_node;
-            ast.nodes.items[current_node] = .{ .fn_param = .{
+            ast.nodes.items[current_node].fn_param = .{
                 .param = node,
                 .next = null_index,
-            } };
+            };
         } else {
-            ast.nodes.items[current_node].fn_param.next = try ast.addNode();
+            ast.nodes.items[current_node].fn_param.next = try ast.addNode(.fn_param);
             current_node = ast.nodes.items[current_node].fn_param.next;
-            ast.nodes.items[current_node] = .{ .fn_param = .{
+            ast.nodes.items[current_node].fn_param = .{
                 .param = node,
                 .next = null_index,
-            } };
+            };
         }
     }
 
@@ -251,7 +256,7 @@ fn parseErrorUnion(ast: *Ast) ParseError!u32 {
     //     type: u32,
     // },
     // @"error": union(enum(u32)) {
-    //     none,
+    //     inferred,
     //     name: u32,
     //     anon: u32,
     // },
@@ -260,29 +265,40 @@ fn parseErrorUnion(ast: *Ast) ParseError!u32 {
     //     next: u32,
     // },
 
-    const error_union = try ast.addNode();
+    const error_union = try ast.addNode(.error_union);
 
     switch (ast.tokens[ast.peek()].tag) {
-        .asterisk, .l_bracket, .l_brace => {
-            ast.nodes.items[error_union] = .{ .error_union = .{
-                .@"error" = try parseType(ast),
-                .type = null_index,
-            } };
+        .asterisk, .l_bracket => {
+            ast.nodes.items[error_union].error_union = .{
+                .@"error" = null_index,
+                .type = try parseType(ast),
+            };
         },
         .octothorpe => {
             _ = ast.pop();
-            ast.nodes.items[error_union] = .{ .error_union = .{
+            const @"error" = try ast.addNode(.@"error");
+            ast.nodes.items[@"error"].@"error" = .{.inferred};
+            ast.nodes.items[error_union].error_union = .{
                 .@"error" = null_index,
                 .type = try parseType(ast),
-            } };
+            };
         },
         .identifier => {
             const first = try parseScopedIdentifier(ast);
-            _ = first; // autofix
             if (ast.tokens[ast.peek()].tag == .octothorpe) {
                 _ = ast.pop();
                 const second = try parseScopedIdentifier(ast);
-                _ = second; // autofix
+                const @"error" = try ast.addNode(.@"error");
+                ast.nodes.items[@"error"].@"error" = .{ .name = first };
+                ast.nodes.items[error_union].error_union = .{
+                    .@"error" = @"error",
+                    .type = second,
+                };
+            } else {
+                ast.nodes.items[error_union].error_union = .{
+                    .@"error" = null_index,
+                    .type = first,
+                };
             }
         },
         else => {
@@ -294,23 +310,46 @@ fn parseErrorUnion(ast: *Ast) ParseError!u32 {
     return error_union;
 }
 
+fn parseStatement(ast: *Ast) ParseError!u32 {
+    _ = ast; // autofix
+}
+
+fn parseBlock(ast: *Ast) ParseError!u32 {
+    try expectToken(ast, .l_brace);
+    if (ast.tokens[ast.peek()].tag == .r_brace) {
+        _ = ast.pop();
+        return null_index;
+    }
+
+    const root = try ast.addNode(.block_member);
+    var current = root;
+    _ = &current; // autofix
+
+    while (ast.tokens[ast.peek()].tag != .r_brace) {
+        ast.nodes.items[current].block_member = .{
+            .statement = try parseStatement(ast),
+            .next = ast.addNode(.block_member),
+        };
+    }
+}
+
 fn parseFn(ast: *Ast) ParseError!u32 {
     // Construct things
-    const root = try ast.addNode();
-    const fn_id_params = try ast.addNode();
-    const fn_rtype_block = try ast.addNode();
-    ast.nodes.items[root] = .{ .fn_decl = .{
+    const root = try ast.addNode(.fn_decl);
+    const fn_id_params = try ast.addNode(.fn_id_params);
+    const fn_rtype_block = try ast.addNode(.fn_rtype_block);
+    ast.nodes.items[root].fn_decl = .{
         .fn_id_params = fn_id_params,
         .fn_rtype_block = fn_rtype_block,
-    } };
-    ast.nodes.items[fn_id_params] = .{ .fn_id_params = .{
+    };
+    ast.nodes.items[fn_id_params].fn_id_params = .{
         .token = undefined,
         .params = null_index,
-    } };
-    ast.nodes.items[fn_rtype_block] = .{ .fn_rtype_block = .{
+    };
+    ast.nodes.items[fn_rtype_block].fn_rtype_block = .{
         .error_union = undefined,
         .block = undefined,
-    } };
+    };
 
     const name = ast.pop();
     ast.nodes.items[fn_id_params].fn_id_params.token = name;
@@ -323,6 +362,7 @@ fn parseFn(ast: *Ast) ParseError!u32 {
     ast.nodes.items[fn_id_params].fn_id_params.params = try parseFnArgs(ast);
 
     ast.nodes.items[fn_rtype_block].fn_rtype_block.error_union = try parseErrorUnion(ast);
+    ast.nodes.items[fn_rtype_block].fn_rtype_block.block = try parseBlock(ast);
 
     // fn_decl: struct {
     //     fn_id_params: u32,
@@ -356,7 +396,7 @@ fn parseFn(ast: *Ast) ParseError!u32 {
     //     qualifier: enum(u32) { none, list, map, reference },
     // },
 
-    return 0xAAAAAAAA;
+    return root;
 }
 
 // ModuleMembers <- (FunctionDecl / EnumDecl / InterfaceDecl / StructDecl / VarDecl
@@ -388,11 +428,11 @@ fn parseModuleMembers(
         },
         .keyword_fn => {
             const function = try parseFn(ast);
-            root_index = try ast.addNode();
-            ast.nodes.items[root_index] = .{ .module_member = .{
+            root_index = try ast.addNode(.module_member);
+            ast.nodes.items[root_index].module_member = .{
                 .member = function,
                 .next = null_index,
-            } };
+            };
         },
         else => std.debug.panic("bad input and/or thing not implemented", .{}),
     };
